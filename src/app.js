7080f9926112344c47a2984376968baf60238892
lfresnog <luisfresno99@gmail.com>
2019-10-17 18:54:04 +0200

    Ten

--- src/app.js
+++ src/app.js
@@ -1,54 +1,75 @@
-// query{
-//   me
-//   hello(name: "Luis")
-//   nombre
-// }
-
 import { fetchData } from './fetchdata';
+import fs from 'fs';
 import { GraphQLServer } from 'graphql-yoga'
+import { getArgumentValues } from 'graphql/execution/values';
 
 // rickymorty entry point
 const url = 'https://rickandmortyapi.com/api/character/';
 
-const typeDefs = `
-
-type Query {
-
-  hello(name: String!): String!,
-  me:String!
-  nombre:[String!]!
-
-}
-`
-
-const resolvers = {
-
-  Query:{
-
-    hello: (parent,args,ctx,info) => {return `Hello ${args.name}`},
-    me: () => "Hola Paco",
-    nombre:( )=>["Hola"," de que vas"]
-
-  },
-
-}
-
 /**
  * Main App
  * @param data all rickyandmorty database
- */ 
-
+ */
 
 const runApp = data => {
-  // data.forEach(element => {
-  //   console.log(`${element.id}: ${element.name}`);
-  // });
+  const typeDefs = 
+  `
+  type Query {
+    character(id: Int ):Character
+    characters(page: Int , pageSize: Int, name: String, status: String, planet: String):[Character]
+    planets: [String!]!
+  }
 
+  type Character {
+    id:Int!
+    name:String!
+    status:String!
+    planet:String!
+  }
+  `
+  const resolvers = {
+  Query:{
+    character: (parent,args,ctx,infor) => {
+      if(args.id){
+        let found1 = data.find(cha => cha.id === args.id);
+      return{
+        id: found1.id, 
+        name: found1.name, 
+        status: found1.status, 
+        planet:found1.location.name
+      }
+      }
+    },
+    characters: (parent,args,ctx,infor) => {
+      const page = args.page || 1;
+      const pageSize = args.pageSize || 20;
+      let found2 = data.filter(cha => cha.name.includes(args.name || cha.name))
+                   data.filter(cha => cha.status.includes(args.status || cha.status))
+                   data.filter(cha => cha.location.name.includes(args.planet || cha.location.name))
+
+      let found = found2.slice((page-1)*pageSize, ((page-1)*pageSize)+(pageSize+1));
+      return found.map(cha => {
+        return{
+          id: cha.id, 
+          name: cha.name, 
+          status: cha.status, 
+          planet: cha.location.name
+        }
+      })
+    },
+    planets: (parent,args,ctx,info) => {
+      const planets = data.forEach(pla =>{
+            planets.push(pla.location.name)
+          })
+          console.log(planets);
+          return [...new Set(planets)];
+    }
+   
+  },
+}
   const server = new GraphQLServer({typeDefs,resolvers});
   server.start({port:"3000"});
 };
-
-
 
 // main program
 fetchData(runApp, url);
--- src/fetchdata.js
+++ src/fetchdata.js
@@ -1,17 +1,28 @@
 import request from 'request';
-
+import fs from 'fs';
+import chalk from 'chalk'
 
 const fetchData = (callback, url, data) => {
   if (!data) data = [];
-  console.log('fechting data...');
-  request({ url, json: true }, (error, response) => {
-    if (response.body) {
-      data = [...data, ...response.body.results];
-    }
-    if (response.body.info.next !== '')
-      fetchData(callback, response.body.info.next, data);
-    else callback(data);
-  });
+  try{
+    data = JSON.parse(fs.readFileSync("./data.txt").toString());
+    console.log(`\n${chalk.yellow("JSON already create")}`);
+    callback(data);
+  }catch(e){
+    console.log('fechting data...');
+    request({ url, json: true }, (error, response) => {
+      if (response.body) {
+        data = [...data, ...response.body.results];
+      }
+      if (response.body.info.next !== '')
+        fetchData(callback, response.body.info.next, data);
+      else {
+        fs.writeFileSync("./data.txt",JSON.stringify(data));
+        console.log(`\n${chalk.yellow("JSON created")}`);
+        callback(data);
+      } 
+    });
+  }
 };
 
 export { fetchData };
